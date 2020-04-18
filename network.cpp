373f6a2aeefae990d95a4a2a1147bcef58ed6a24
AarronRasmussen <arasmussen96@gmaill.com>
2020-04-17 23:14:41 -0600

    Removed bug when creating Layers

--- network.cpp
+++ network.cpp
@@ -1,12 +1,9 @@
 #include "network.h"
 
 Network::Network(const std::vector<int>& n) {
-    auto iter = n.begin() + 1;
-
-    std::for_each(n.begin(), n.end(), [&](int num){
-        layers.push_back(Layer(num, *iter));
-        iter++;
-    });
+    for(int i = 1; i < n.size(); i++) {
+        layers.push_back(Layer(n.at(i), n.at(i - 1)));
+    }
 }
 
 void Network::printNetWork() {
